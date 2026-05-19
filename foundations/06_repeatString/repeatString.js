7d003f40ea7ac25591b35597c9322a9891f23b04
ptejasv <e0564194@u.nus.edu>
2026-05-19 16:47:47 +0800

    feat: completed foundations/06_repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, numRepeats) {
+    if (numRepeats < 0) return 'ERROR';
+    let res = "";
+    for (numRepeats; numRepeats > 0; numRepeats--) {
+        res += str;
+    }
+    return res;
 };
 
 // Do not edit below this line
