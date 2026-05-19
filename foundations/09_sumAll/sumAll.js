8846c68fbbab02415e0fb995e567b35da454d2fc
ptejasv <e0564194@u.nus.edu>
2026-05-19 18:12:39 +0800

    feat: completed foundations/09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if ((a < 1 || b < 1) || !(Number.isInteger(a) && Number.isInteger(b))) return 'ERROR';
+    if (a > b) [a, b] = [b, a];
+    let sum = 0;
 
+    for (let i = a; i <= b; i++) {
+        sum += i;
+    }
+
+    return sum;
+    //return ((end - start + 1) / 2) * (start + end) for smartypants
 };
 
 // Do not edit below this line
