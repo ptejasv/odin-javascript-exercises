382e05ae2e0b62717671ee85d5ffe14c6dc7c4de
ptejasv <e0564194@u.nus.edu>
2026-05-19 18:33:00 +0800

    feat: completed foundations/11_tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+    return Number(Math.round(((temp - 32) * (5/9)) + 'e1') + 'e-1');
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+    return Number(Math.round(((temp * (9/5)) + 32) + 'e1') + 'e-1');
 };
 
 // Do not edit below this line
