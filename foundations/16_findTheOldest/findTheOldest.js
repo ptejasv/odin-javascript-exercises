2ec46ffb5e5b38356c73d7948215de9c6f135298
ptejasv <e0564194@u.nus.edu>
2026-07-05 00:45:12 +0800

    feat: completed foundations/16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldestAge = -1; // keep track of the oldest's age
 
+    return people.reduce(
+        (oldest, current) => {
+            let currentAge = 0;
+            if (!("yearOfDeath" in current)) {
+                // if the year of death is not specified, take the
+                // current year
+                currentAge = (new Date).getFullYear() - current.yearOfBirth;
+            } else {
+                currentAge = current.yearOfDeath - current.yearOfBirth;
+            }
+
+            // compare the current person's age to the 
+            // age of the oldest person
+            if (currentAge > oldestAge) {
+                oldestAge = currentAge;
+                return current;
+            }
+            return oldest;
+        }, people[0]
+    )
 };
 
 // Do not edit below this line
