a3c9db7f0dce871fa671bd2813af85fa0df1a7c0
ptejasv <e0564194@u.nus.edu>
2026-07-04 20:17:49 +0800

    feat: completed foundations/14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = Number(n); 
 
+    if (n < 0) return "OOPS";
+    if (n <= 1) return n;
+
+    let first = 0, second = 1, current = 0;
+
+    // iterative bottom-up approach
+    while (n-- > 1) {
+        current = first + second;
+        first = second;
+        second = current;
+    }
+
+    return current;
 };
 
 // Do not edit below this line
