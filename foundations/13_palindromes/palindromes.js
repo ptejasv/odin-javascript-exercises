7849da3248e2ee0b83e7a1493b47353530e3d293
ptejasv <e0564194@u.nus.edu>
2026-07-04 15:31:16 +0800

    feat: completed foundations/13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    // clean up punctuation and capitalisation inconsistencies
+    str = str.toLowerCase().replace(/\W/g, "").replace();
 
-};
+    // 2 pointer iteration
+    for (let i = 0; i < Math.floor(str.length/2); i++) {
+        if (str[i] !== str[str.length - 1 - i]) return false;
+    };
+
+    return true;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
